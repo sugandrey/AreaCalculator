60e5ea53aad9cd24636f5d23797e0d61e86cac8a
sugandrey <sugandrey1@gmail.com>
2021-03-18 14:38:03 -0400

    adding files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    //psvm - output main method;
+    public static void main(String[] args) {
+        double printArea = AreaCalculator.area(-1);
+        double printAreaOne = AreaCalculator.area(-1,4);
+        System.out.println("S = "+ printArea);
+        System.out.println("S = "+ printAreaOne);
+    }
+}
